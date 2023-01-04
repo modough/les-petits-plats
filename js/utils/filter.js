4be2d95d1c4b65ae6d081d543773513ec74ed242
mouhamed <mouhamed.mbaye1@outlook.com>
2023-01-04 15:32:35 +0100

    filter function in progress

--- js/utils/filter.js
+++ js/utils/filter.js
@@ -1,25 +1,16 @@
 export const filter = () => {
 	const span = document.querySelectorAll('li.ingredient');
 
-	const card = document.querySelectorAll('.cardIngredients');
+	const name = document.querySelectorAll('.ingredientName');
+	const description = document.querySelectorAll('.ingredientDescription');
 
 	span.forEach((elmt) => {
 		elmt.addEventListener('click', (e) => {
-			console.log(e.target);
-
-			card.forEach((elmt) => {
-				for (let i in elmt.children) {
-					const firstChild = elmt.children[i].firstChild.innerHTML;
-					console.log(firstChild.split(':').join(' '));
-					if (firstChild.split(':').join(' ') == e.target.innerHTML) {
-						const section = document.querySelector('.recipesCards');
-						section.innerHTML = '';
-						const parentCard = document.querySelector('.card');
-						parentCard.appendChild(elmt);
-						section.appendChild(parentCard);
-					}
-				}
-			});
+			const value = e.target.innerHTML;
+			if (name.innerHTML === value || description.innerHTML === value) {
+				const section = document.querySelector('.recipesCards');
+				section.innerHTML = '';
+			}
 		});
 	});
 };
