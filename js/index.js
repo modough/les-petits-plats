061b1fd26756c140665ab9f538776e23dedfef53
mouhamed <mouhamed.mbaye1@outlook.com>
2023-01-30 23:08:31 +0100

    creating no list message

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { listFactory } from './factories/listFactory';
 import toggleLists from './utils/toggleLists';
 import recipes from './data/recipes';
 import filter from './utils/filter';
+import createElementDOM from './utils/genericDom';
 
 
 //display all recipes
@@ -14,17 +15,24 @@ import filter from './utils/filter';
 export const displayRecipes = (data) => {
 	const recipeModel = recipeFactory(data);
 	const recipeCardDOM = recipeModel.getRecipeCardDOM();
-	buildRecipeCardDOM(recipeCardDOM);
+	buildRecipeCardDOM(recipeCardDOM, data);
 };
 
 /**
  * The buildRecipeCardDOM function takes in a recipe object and returns a recipe card element.
  * @param element - the HTML element that you want to append to the DOM
  */
-const buildRecipeCardDOM = (element) => {
+const buildRecipeCardDOM = (element, data) => {
 	const section = document.querySelector('.recipesCards');
+	const noList = createElementDOM('p', ` Aucune recette ne correspond à votre critère… vous pouvez
+	chercher « tarte aux pommes », « poisson », etc.
+	`, 'noListDiv');
 	section.innerHTML = '';
 	section.appendChild(element);
+	section.appendChild(noList);
+	if (data.length === 0) {
+		noList.classList.add('active');
+	}
 };
 
 
